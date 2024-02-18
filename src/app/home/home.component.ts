5fe66ad2b872b0b241ceba3bed79e8cb7d34b239
Karylle <keivelarde@gmail.com>
2024-02-18 21:02:01 +0800

    finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 
 import { StyleClassModule } from 'primeng/styleclass';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [StyleClassModule, ButtonModule],
+  imports: [StyleClassModule, ButtonModule, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
